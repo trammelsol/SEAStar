68d8887b1bf359a0337e8fbf4ed448f5c0142b7c
Trammels <m15587450902@163.com>
2022-03-22 08:56:56 +0800

    Proxy finished !

--- Proxy/Main.cpp
+++ Proxy/Main.cpp
@@ -4,13 +4,13 @@
 int main(){
 	//auto proxy = std::make_unique<Proxy>();
 	std::unique_ptr<Proxy>proxy(new Proxy());
-	int a = proxy->ProxyGenerate("Pig");//1469
-	std::cout << a << endl;
-	int b = proxy->ProxyGenerate("Dog");//2057
-	std::cout << b << endl;
-	int c = proxy->ProxyGenerate("Dog");//2057
-	std::cout << c << endl;
-	int d = proxy->ProxyGenerate("Tiger");//1368
-	std::cout << d << endl;
+	int pig = proxy->ProxyGenerate("Pig");//1469
+	std::cout << pig << endl;
+	int dog = proxy->ProxyGenerate("Dog");//2057
+	std::cout << dog << endl;
+	int DogAdd = proxy->ProxyGenerate("Dog");//2057
+	std::cout << DogAdd << endl;
+	int tiger = proxy->ProxyGenerate("Tiger");//1368
+	std::cout << tiger << endl;
     return 0;
 }
